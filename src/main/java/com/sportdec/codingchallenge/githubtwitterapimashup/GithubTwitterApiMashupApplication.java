aae5b9311fb5e85a538b7b633b15c5c75a5e1315
Martin Kempf <martin.kempf@gmail.com>
2018-01-23 22:40:39 +0100

    init spring boot application

---
+++ src/main/java/com/sportdec/codingchallenge/githubtwitterapimashup/GithubTwitterApiMashupApplication.java
@@ -0,0 +1,12 @@
+package com.sportdec.codingchallenge.githubtwitterapimashup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTwitterApiMashupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTwitterApiMashupApplication.class, args);
+	}
+}
